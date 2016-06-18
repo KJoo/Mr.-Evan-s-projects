39342e844777062b1e599559eceab7ff3e874dc0
KJoo <jookyle91@gmail.com>
2016-06-17 20:35:32 -0700

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include "Date.h"
+
+using namespace std;
+
+int main()
+{
+	Date a;
+	cout << a.month << "/" << a.day << "/" << a.year << endl;
+
+	cout << "Happy Birthday! I love your face and feet!!!" << endl;
+}
